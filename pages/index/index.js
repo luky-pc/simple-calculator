8ceef629d90d3ad6019106f9ad0848922dff6aa9
zhangpengcheng@eqxiu.com <zhangpengcheng@eqxiu.com>
2022-06-20 17:21:58 +0800

    初始化计算器小程序
    添加基础计算功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,8 @@ const app = getApp()
 Page({
   data: {
     currentVal: '',
+    actionArr: [],
+    numberArr: [],
   },
   // 事件处理函数
   bindViewTap() {
@@ -18,6 +20,71 @@ Page({
     const number = e.currentTarget.dataset['number'];
     this.setData(
       {currentVal: this.data.currentVal + number}
+    );
+  },
+  setDot: function () {
+    const currentVal = this.data.currentVal;
+    if (currentVal.indexOf('.') === -1) {
+      this.setData(
+        {currentVal: this.data.currentVal + '.'}
+      );
+    }
+  },
+  setAction: function (e) {
+    const { currentVal, numberArr, actionArr} = this.data;
+    const action = e.currentTarget.dataset['action'];
+    if (currentVal) {
+      const lastNumber = parseFloat(numberArr.pop());
+      const lastAction = actionArr.pop();
+      switch (lastAction) {
+        case '*': 
+          numberArr.push(lastNumber * parseFloat(currentVal));
+          break;
+        case '/':
+          numberArr.push(lastNumber / parseFloat(currentVal));
+          break;
+        default:
+          !isNaN(lastNumber) && numberArr.push(lastNumber);
+          numberArr.push(currentVal);
+          lastAction && actionArr.push(lastAction);
+          break;
+      }
+      actionArr.push(action);
+    } else { // 连续输入 操作符 认为前一个操作符 无效
+      actionArr.pop();
+      actionArr.push(action);
+    }
+    this.setData(
+      {
+        numberArr: numberArr,
+        actionArr: actionArr,
+        currentVal: '',
+      }
+    );
+  },
+  calcResult: function (e) {
+    let result;
+    const { currentVal, numberArr, actionArr} = this.data;
+    if (!numberArr.length) return; 
+    result = parseFloat(numberArr.shift());
+    numberArr.push(currentVal);
+    while(numberArr.length) {
+      let action = actionArr.shift();
+      let current = parseFloat(numberArr.shift());
+      switch (action) {
+        case '+': result += current;
+          break;
+        case '-': result-= current;
+          break;
+        case '*': result *= current;
+          break;
+        case '/': result /= current;
+          break;
+        default: break;
+      }
+    }
+    this.setData(
+      { currentVal: result }
     );
   }
 })
